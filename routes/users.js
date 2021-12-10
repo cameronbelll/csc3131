ac8414d646373843b8df028ef5fe8c918582ab77
cameronbelll <77004099+cameronbelll@users.noreply.github.com>
2021-12-10 16:18:31 +0000

    added middleware, updated other files

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import {signIn, signUp} from '../controllers/user.js';
+
+const router = express.Router();
+
+router.post('/signIn', signIn); //this is a post route as it is sending data (details from form) to the backend
+router.post('/signup', signUp);
+
+export default router;
